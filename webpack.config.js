6e4761652d7e350f10a968ded301bb9cec0dc913
Yuri Pavlov <yuripavlovdex@gmail.com>
2022-12-06 19:25:07 +0300

    fix svg

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ module.exports = {
         }
       },
       {
-        test: /\.(jpe?g|png|webp|gif|svg)$/i,
+        test: /\.(jpe?g|png|webp|gif)$/i,
         use: [
           {
             loader: 'image-webpack-loader',
